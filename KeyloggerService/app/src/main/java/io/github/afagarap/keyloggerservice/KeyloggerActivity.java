958ad1605a3c7f578f830617903a553e9d9f35cf
AFAgarap <abienfred.agarap@yahoo.com>
2017-07-20 00:25:35 +0800

    Created project for a keylogger service

---
+++ KeyloggerService/app/src/main/java/io/github/afagarap/keyloggerservice/KeyloggerActivity.java
@@ -0,0 +1,13 @@
+package io.github.afagarap.keyloggerservice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KeyloggerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keylogger);
+    }
+}
