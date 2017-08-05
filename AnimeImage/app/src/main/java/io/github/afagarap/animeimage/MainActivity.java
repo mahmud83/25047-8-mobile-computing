e3fa00b7bf9f1cb18eef7c564fda6c1ebaa0b61e
AFAgarap <abienfred.agarap@yahoo.com>
2017-08-05 22:13:42 +0800

    August 5, 2017 laboratory activity. Fade-in, fade-out loop of two ImageViews

---
+++ AnimeImage/app/src/main/java/io/github/afagarap/animeimage/MainActivity.java
@@ -0,0 +1,44 @@
+package io.github.afagarap.animeimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+/**
+ * Created by darth 08/05/2017
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Fade out imageFirefox, fade in imageChrome
+     * Uses ternary operator for setting the image alpha
+     * @param view
+     */
+    public void animateImage(View view) {
+        ImageView imageFirefox = (ImageView) findViewById(R.id.imageFirefox);
+        ImageView imageChrome = (ImageView) findViewById(R.id.imageChrome);
+        imageFirefox.animate()
+                .rotationBy(3600)
+                .alpha(imageFirefox.getAlpha() == 0 ? 1 : 0)
+                .setDuration(2000);
+        imageChrome.animate()
+                .rotationBy(-3600)
+                .alpha(imageChrome.getAlpha() == 1 ? 0 : 1)
+                .setDuration(2000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageFirefox = (ImageView) findViewById(R.id.imageChrome);
+        imageFirefox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                animateImage(view);
+            }
+        });
+    }
+}
