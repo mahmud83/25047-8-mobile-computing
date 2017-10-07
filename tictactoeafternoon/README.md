a3d235e9eefc3f6561f688bc5fcb807dc9c38fe1
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-10-07 19:54:19 +0800

    Latest build of tictactoe

--- tictactoeafternoon/README.md
+++ tictactoeafternoon/README.md
@@ -1,4 +1,4 @@
 # Tic-Tac-Toe
-### by A.F. Agarap
+### by A.F. Agarap & P. Esperanza
 
-This is a laboratory activity for September 2, 2017 for 25048 Mobile Computing in Adamson University (1st semester, 2017-2018). The objective of this activity is to develop a Tic-Tac-Toe app using RelativeLayout and ImageViews.
+This is a laboratory activity for September 23, 2017 for 25048 Mobile Computing in Adamson University (1st semester, 2017-2018). The objective of this activity is to develop a Tic-Tac-Toe app using RelativeLayout and ImageViews.
--- tictactoeafternoon/app/src/main/java/mobilecomputinglab/tictactoeafternoon/MainActivity.java
+++ tictactoeafternoon/app/src/main/java/mobilecomputinglab/tictactoeafternoon/MainActivity.java
@@ -26,11 +26,18 @@ package mobilecomputinglab.tictactoeafternoon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     // number of board cells
     public static final int numCells = 9;
@@ -38,11 +45,24 @@ public class MainActivity extends AppCompatActivity {
     // indicator of whose turn it is
     public boolean isTurn0 = true;
 
+    // indicator of whether the game is over
+    public boolean isOver = false;
+
     // ImageView objects for each board cell
     public ImageView cell0, cell1, cell2, cell3, cell4, cell5, cell6, cell7, cell8;
 
     // ImageView array to contain all board cells
     public ImageView[] imageCells = new ImageView[numCells];
+
+    public int[][] matrix = new int[3][3];
+    public int scoreJake;
+    public int scoreFinn;
+
+    public Button buttonRestart;
+    public Button buttonPlayAgain;
+
+    public TextView textScoreJake;
+    public TextView textScoreFinn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,16 +94,66 @@ public class MainActivity extends AppCompatActivity {
         imageCells[7] = cell7;
         imageCells[8] = cell8;
 
+        textScoreFinn = (TextView) findViewById(R.id.textScoreFinn);
+        textScoreJake = (TextView) findViewById(R.id.textScoreJake);
+
         // give ImageView element an OnTouchListener
         for (int index = 0; index < numCells; index++) {
             imageCells[index].setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
-                    commitMove(v);
+                    if (!isOver) commitMove(v);
                     return true;
                 }
             });
         }
+
+        buttonPlayAgain = (Button) findViewById(R.id.buttonPlayAgain);
+        buttonPlayAgain.setVisibility(View.INVISIBLE);
+        buttonPlayAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonRestart.setVisibility(View.INVISIBLE);
+                buttonPlayAgain.setVisibility(View.INVISIBLE);
+                isTurn0 = true;
+                isOver = false;
+                scoreFinn = 0;
+                scoreJake = 0;
+                textScoreFinn.setText("Finn:");
+                textScoreJake.setText("Jake:");
+
+                for (int row = 0; row < 3; row++) {
+                    for (int col = 0; col < 3; col++) {
+                        matrix[row][col] = 0;
+                    }
+                }
+
+                for (int index = 0; index < 9; index++) {
+                    imageCells[index].setAlpha(0f);
+                }
+            }
+        });
+
+        buttonRestart = (Button) findViewById(R.id.buttonRestart);
+        buttonRestart.setVisibility(View.INVISIBLE);
+        buttonRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonRestart.setVisibility(View.INVISIBLE);
+                isTurn0 = true;
+                isOver = false;
+
+                for (int row = 0; row < 3; row++) {
+                    for (int col = 0; col < 3; col++) {
+                        matrix[row][col] = 0;
+                    }
+                }
+
+                for (int index = 0; index < 9; index++) {
+                    imageCells[index].setAlpha(0f);
+                }
+            }
+        });
     }
 
     /**
@@ -99,14 +169,73 @@ public class MainActivity extends AppCompatActivity {
             view.setAlpha(1);
         }
 
+        int objectId = view.getId();
+
         // placeholder for image
         ImageView imageCell = (ImageView) view;
 
         // set image resource based on whose turn it is
-        imageCell.setImageResource(isTurn0 ? R.drawable.red : R.drawable.yellow);
+        imageCell.setImageResource(isTurn0 ? R.drawable.jake : R.drawable.finn);
+
+        switch (objectId) {
+            case R.id.imageCell0: matrix[0][0] = (isTurn0) ? 1 : 2; break;
+            case R.id.imageCell1: matrix[0][1] = (isTurn0) ? 1 : 2; break;
+            case R.id.imageCell2: matrix[0][2] = (isTurn0) ? 1 : 2; break;
+            case R.id.imageCell3: matrix[1][0] = (isTurn0) ? 1 : 2; break;
+            case R.id.imageCell4: matrix[1][1] = (isTurn0) ? 1 : 2; break;
+            case R.id.imageCell5: matrix[1][2] = (isTurn0) ? 1 : 2; break;
+            case R.id.imageCell6: matrix[2][0] = (isTurn0) ? 1 : 2; break;
+            case R.id.imageCell7: matrix[2][1] = (isTurn0) ? 1 : 2; break;
+            default: matrix[2][2] = (isTurn0) ? 1 : 2; break;
+        }
+
+        if (isWin(isTurn0 ? 1 : 2)) {
+            Toast.makeText(this, String.format(Locale.ENGLISH, "Winner: Player %s", isTurn0 ? "Jake" : "Finn"),
+                    Toast.LENGTH_SHORT).show();
+
+            if (isTurn0) {
+                scoreJake += 1;
+                textScoreJake.setText("Jake: " + scoreJake);
+            } else {
+                scoreFinn += 1;
+                textScoreFinn.setText("Finn: " + scoreFinn);
+            }
+
+            isOver = true;
+            buttonRestart.setVisibility(View.VISIBLE);
+            buttonPlayAgain.setVisibility(View.VISIBLE);
+        } else if (isBoardFull()) {
+            Toast.makeText(this, "Draw!", Toast.LENGTH_SHORT).show();
+            isOver = true;
+            buttonRestart.setVisibility(View.VISIBLE);
+            buttonPlayAgain.setVisibility(View.VISIBLE);
+        }
 
         // boolean value for setting whose turn it is
         isTurn0 = !isTurn0;
+
+        // log whose turn it is
+        Log.d(TAG, String.format(Locale.ENGLISH, "Turn 0 : %s", isTurn0));
+    }
+
+    public boolean isBoardFull() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                if (matrix[row][col] == 0) return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isWin(int move) {
+        return ((matrix[0][0] == move && matrix[0][1] == move && matrix[0][2] == move) ||
+                (matrix[1][0] == move && matrix[1][1] == move && matrix[1][2] == move) ||
+                (matrix[2][0] == move && matrix[2][1] == move && matrix[2][2] == move) ||
+                (matrix[0][0] == move && matrix[1][0] == move && matrix[2][0] == move) ||
+                (matrix[0][1] == move && matrix[1][1] == move && matrix[2][1] == move) ||
+                (matrix[0][2] == move && matrix[1][2] == move && matrix[2][2] == move) ||
+                (matrix[0][0] == move && matrix[1][1] == move && matrix[2][2] == move) ||
+                (matrix[0][2] == move && matrix[1][1] == move && matrix[2][0] == move));
     }
 
 }
