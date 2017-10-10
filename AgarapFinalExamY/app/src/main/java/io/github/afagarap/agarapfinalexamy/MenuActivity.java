689f7ff6c5bb5fa693d774e607a84ca23c09ede6
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-10-10 13:36:59 +0800

    Gets the first name from intent extras, appends it to the greetings textview

--- AgarapFinalExamY/app/src/main/java/io/github/afagarap/agarapfinalexamy/MenuActivity.java
+++ AgarapFinalExamY/app/src/main/java/io/github/afagarap/agarapfinalexamy/MenuActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -12,6 +15,10 @@ public class MenuActivity extends AppCompatActivity {
     private Button mAtmButton;
     private Button mTriangularButton;
     private Button mTictactoeButton;
+
+    private TextView mTextViewGreeting;
+
+    private String mFirstName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,13 @@ public class MenuActivity extends AppCompatActivity {
         mAtmButton = (Button) findViewById(R.id.buttonATM);
         mTriangularButton = (Button) findViewById(R.id.buttonTriangular);
         mTictactoeButton = (Button) findViewById(R.id.buttonTictactoe);
+
+        mTextViewGreeting = (TextView) findViewById(R.id.textViewGreetings);
+
+        Intent intent = getIntent();
+        mFirstName = intent.getExtras().getString(Constants.FIRSTNAME);
+        mTextViewGreeting.setText(String.format(Locale.ENGLISH, "%s %s",
+                mTextViewGreeting.getText(), mFirstName));
 
         mAnimationButton.setOnClickListener(new View.OnClickListener() {
             @Override
