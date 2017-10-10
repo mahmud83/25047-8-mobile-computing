dabbddaf926156637b3ad806e5a3f1060a841024
AFAgarap <abien.fred.agarap@adamson.edu.ph>
2017-10-10 19:40:52 +0800

    Uses one textview to display the account balance and its label; local declaration of button for checking balance, and float variable for account balance.

--- AgarapFinalExamY/app/src/main/java/io/github/afagarap/agarapfinalexamy/BalanceActivity.java
+++ AgarapFinalExamY/app/src/main/java/io/github/afagarap/agarapfinalexamy/BalanceActivity.java
@@ -9,13 +9,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class BalanceActivity extends AppCompatActivity {
 
-    private TextView mAccountBalanceLabelTextView, mAccountBalanceTextView;
+    private TextView mAccountBalanceLabelTextView;
     private EditText mAccountPinEditText;
-    private Button mCheckBalanceButton;
 
-    private float mAccountBalance;
     private String mAccountPin;
 
     @Override
@@ -24,25 +24,24 @@ public class BalanceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_balance);
 
         mAccountBalanceLabelTextView = (TextView) findViewById(R.id.mAccountBalanceLabelTextView);
-        mAccountBalanceTextView = (TextView) findViewById(R.id.mAccountBalanceTextView);
+
         mAccountPinEditText = (EditText) findViewById(R.id.accountPinEditText);
-        mCheckBalanceButton = (Button) findViewById(R.id.checkBalanceButton);
+        Button mCheckBalanceButton = (Button) findViewById(R.id.checkBalanceButton);
 
         mAccountBalanceLabelTextView.setVisibility(View.INVISIBLE);
-        mAccountBalanceTextView.setVisibility(View.INVISIBLE);
 
         Intent intent = getIntent();
-        mAccountBalance = intent.getExtras().getFloat(Constants.ACCOUNT_BALANCE);
+        float mAccountBalance = intent.getExtras().getFloat(Constants.ACCOUNT_BALANCE);
         mAccountPin = intent.getExtras().getString(Constants.ACCOUNT_PIN);
 
-        mAccountBalanceTextView.setText(mAccountBalance + "");
+        mAccountBalanceLabelTextView.setText(String.format(Locale.ENGLISH, "%s : %s",
+                mAccountBalanceLabelTextView.getText(), mAccountBalance));
 
         mCheckBalanceButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mAccountPinEditText.getText() != null && mAccountPinEditText.getText().toString().equals(mAccountPin)) {
                     mAccountBalanceLabelTextView.setVisibility(View.VISIBLE);
-                    mAccountBalanceTextView.setVisibility(View.VISIBLE);
                 } else {
                     Toast.makeText(BalanceActivity.this, "Invalid Pin", Toast.LENGTH_SHORT).show();
                 }
